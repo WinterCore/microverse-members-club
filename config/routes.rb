1d4bcc6f4324c3a13272e54e98d2d6d7cd186068
Bismarck-GM <geromorla@yahoo.com.ar>
2020-07-23 13:40:49 -0300

    Add devise controller for users. Allow username params to create and update user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
