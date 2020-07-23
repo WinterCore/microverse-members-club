ffc4a44214b2c16b4a9cfccddff1609bd0cdf872
Bismarck-GM <geromorla@yahoo.com.ar>
2020-07-23 14:13:26 -0300

    Only authenticated user can see authors and edit/post posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
