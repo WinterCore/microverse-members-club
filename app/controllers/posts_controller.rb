36118684c3972d4773bbbb45b43c2602f3303cf9
WinterCore <hogobbl@gmail.com>
2020-07-24 16:38:14 +0200

    Fixed more bugs and security issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[show index]
   before_action :set_posts, only: %i[index create edit]
+  before_action :authorize_user, only: %i[edit update destroy]
 
   def index
-    @post = Post.new
-  end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show; end
-
-  # GET /posts/new
-  def new
     @post = Post.new
   end
 
@@ -40,25 +32,25 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_posts
     @posts = Post.order('created_at DESC')
   end
 
-  # Only allow a list of trusted parameters through.
+  def authorize_user
+    return unless @post.user_id != current_user.id
+    flash[:alert] = 'You\'re not supposed to be here'
+    redirect_to posts_path
+  end
+
   def post_params
     params.require(:post).permit(:body, :user_id)
   end
